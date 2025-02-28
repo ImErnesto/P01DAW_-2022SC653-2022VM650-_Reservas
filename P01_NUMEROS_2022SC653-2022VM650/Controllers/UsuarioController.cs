f17fc20202a679aaa0d4b5c0d9ad147405d2e206
LuisS-byte <luis.sarmiento@catolica.edu.sv>
2025-02-27 19:26:28 -0600

    commit

---
+++ P01_NUMEROS_2022SC653-2022VM650/Controllers/UsuarioController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P01_NUMEROS_2022SC653_2022VM650.Models;
+
+namespace P01_NUMEROS_2022SC653_2022VM650.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public UsuarioController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("ObtenerUsuarios")]
+        public async Task<IActionResult> ObtenerUsuarios()
+        {
+            var Usuario = await (from u in _context.Usuarios select u).ToListAsync();
+            return Ok(Usuario);
+        }
+
+        [HttpPost("Validar")]
+        public async Task<IActionResult> Validar(string usuario, string contraseña)
+        {
+            var usuarioValidar = await (from u in _context.Usuarios where u.Correo==usuario && u.Contrasena == contraseña select u).ToListAsync();
+            if (usuarioValidar == null)
+            {
+                return NotFound();
+            }
+            return Ok("Validacion exitosa \n Bienvenido: " + usuarioValidar[0].Nombre);
+        }
+
+        [HttpPost]
+        [Route("CrearUsuario")]
+        public async Task<IActionResult> CrearUsuario([FromBody] Usuario usuario)
+        {
+            await _context.Usuarios.AddAsync(usuario);
+            await _context.SaveChangesAsync();
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        [Route("ActualizarUsuario/{id}")]
+        public async Task<IActionResult> ActualizarUsuario(int id, [FromBody] Usuario usuario)
+        {
+            var usuarioActualizar = await _context.Usuarios.FirstOrDefaultAsync(c => c.UsuarioId == id);
+            if (usuarioActualizar == null)
+            {
+                return NotFound();
+            }
+            usuarioActualizar.Nombre = usuario.Nombre;
+            usuarioActualizar.Correo = usuario.Correo;
+            usuarioActualizar.Telefono = usuario.Telefono;
+            usuarioActualizar.Contrasena = usuario.Contrasena;
+            usuarioActualizar.Rol = usuario.Rol;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(usuario);
+        }
+
+        [HttpDelete]
+        [Route("EliminarUsuario/{id}")]
+        public async Task<IActionResult> EliminarUsuario(int id)
+        {
+            var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.UsuarioId == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
